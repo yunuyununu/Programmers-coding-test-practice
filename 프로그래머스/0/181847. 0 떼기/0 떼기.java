02498787e3413c9e9de8f47e0cec0b2867876b84
yunuyununu <156145554+yunuyununu@users.noreply.github.com>
2024-10-09 22:52:18 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = String.valueOf(Integer.parseInt(n_str));
-        return answer;
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
