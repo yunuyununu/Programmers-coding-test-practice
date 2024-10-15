96871fdefd30f0caee30123c0b01ab90526c2677
yunuyununu <156145554+yunuyununu@users.noreply.github.com>
2024-10-16 00:47:03 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1;i<100;i++){
+        for(int i=1;i<=n;i++){
             if((6*i)%n==0){
                 answer = i;
                 break;
