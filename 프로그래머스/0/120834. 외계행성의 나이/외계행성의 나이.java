86f254c631ec33cac3394f34de1537feaff8cf21
yunuyununu <156145554+yunuyununu@users.noreply.github.com>
2024-10-17 15:22:50 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.77 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,30 +1,10 @@
 class Solution {
     public String solution(int age) {
         String answer ="";
-        String[] ag = String.valueOf(age).split("");
+        String[] ag = Integer.toString(age).split("");
+        String[] alp = {"a","b","c","d","e","f","g","h","i","j"};
         for(int i=0;i<ag.length;i++){
-            switch(ag[i]){
-                case "0": answer += "a";
-                    break;
-                case "1": answer += "b";
-                    break;
-                case "2": answer += "c";
-                    break;
-                case "3": answer += "d";
-                    break;
-                case "4": answer += "e";
-                    break;
-                case "5": answer += "f";
-                    break;
-                case "6": answer += "g";
-                    break;
-                case "7": answer += "h";
-                    break;
-                case "8": answer += "i";
-                    break;
-                case "9": answer += "j";
-                    break;
-            }
+            answer += alp[Integer.parseInt(ag[i])];
         }
         return answer;
     }
