bbf17cc13cf1897c6d7424036a20e2043142d15d
yunuyununu <156145554+yunuyununu@users.noreply.github.com>
2024-11-08 19:45:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.17 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean solution(String s) {
+         if (s.length() == 4 || s.length() == 6) {
+            return s.matches("(^[0-9]*$)");
+        }
+        return false;
+    }
+}
