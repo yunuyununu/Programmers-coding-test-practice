e0eefb5ecbf74afd42fe629b563d2411e6207d7e
yunuyununu <156145554+yunuyununu@users.noreply.github.com>
2024-11-18 16:43:56 +0900

    [level 0] Title: 외계어 사전, Time: 0.11 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        int cnt = 0;
+        for(int i=0;i<dic.length;i++){
+            cnt = 0;
+            for(int j=0;j<spell.length;j++){
+                if(dic[i].contains(spell[j])){
+                    cnt++;
+                }
+            }
+            if(cnt == spell.length){
+                answer =1;
+            }
+        }
+        return answer;
+    }
+}
